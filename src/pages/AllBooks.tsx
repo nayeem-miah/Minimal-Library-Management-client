b8de9ee27095e93ebad2948774fd36287ca0e4f6
nayeem-miah <nayeem5113a@gmail.com>
2025-07-23 17:10:53 +0600

    degin updated

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,4 +1,5 @@
 import { BookCard } from "@/components/card/BookCard";
+import { Button } from "@/components/ui/button";
 import { useGetBooksQuery } from "@/redux/api/baseApi"
 import type { IBook } from "@/types";
 
@@ -14,13 +15,20 @@ function AllBooks() {
         <div>
             <div>
                 <h3>Show all books</h3>
+
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {
                         !isLoading && !isError && data?.data?.length > 0 ? (
                             data.data.map((book: IBook) => (
                                 <BookCard book={book} />
                             ))
-                        ) : <div>No books available</div>
+                        ) : <div className="flex flex-col items-center justify-center mx-auto max-w-4xl">
+                            <h3 className="text-red-400 text-2xl text-center">No books available</h3>
+                            <p className="text-gray-600 text-center">There are currently no books available in the library.</p>
+                            <Button className="mt-4 bg-pink-600 " variant="secondary" onClick={() => window.location.href = "/add-books"}>
+                                Add New Book
+                            </Button>
+                        </div>
 
                     }
                 </div>
